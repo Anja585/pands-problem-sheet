784cda681c648131eeac629a9b76fea6285ba024
unknown <G00398256@gmit.ie>
2021-02-08 21:39:55 +0000

    improved layout

--- bmi.py
+++ bmi.py
@@ -4,11 +4,15 @@
 
 # Place height here
 height = input ("Enter height (in cm): ")
+
 # Place weight here
 weight = input ("Enter weight (in kg): ")
+
 # Calculates BMI to a float
 BMI1 = int(weight)/((int(height)/100)**2)
+
 # Rounds BMI to two decimal places
 BMI2 = round (BMI1,2)
+
 # prints the BMI
 print ("BMI is", BMI2)
