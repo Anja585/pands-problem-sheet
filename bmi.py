b694c3f1b5bdf3e1841925222327d4f743fc713c
unknown <G00398256@gmit.ie>
2021-04-13 12:16:39 +0100

    file tidied up

--- bmi.py
+++ bmi.py
@@ -1,18 +1,8 @@
-# bmi.py
 # This program calculates user's Body Mass Index (BMI)
 # Author: Anja Antolkovic
 
-# Place height here
-height = input ("Enter height (in cm): ")
-
-# Place weight here
-weight = input ("Enter weight (in kg): ")
-
-# Calculates BMI to a float
-BMI1 = int(weight)/((int(height)/100)**2)
-
-# Rounds BMI to two decimal places
-BMI2 = round (BMI1,2)
-
-# prints the BMI
-print ("BMI is", BMI2)
+height = input ("Enter height (in cm): ") # Place height here
+weight = input ("Enter weight (in kg): ") # Place weight here
+BMI1 = int(weight)/((int(height)/100)**2) # Calculates BMI to a float
+BMI2 = round (BMI1,2) # Rounds BMI to two decimal places
+print ("BMI is", BMI2) # prints the BMI
