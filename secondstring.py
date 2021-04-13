5c7f0c3c87e2811e6352abd407c37992553a8b59
unknown <G00398256@gmit.ie>
2021-04-13 15:57:54 +0100

    file tidied up, improved code

--- secondstring.py
+++ secondstring.py
@@ -1,17 +1,9 @@
 # secondstring.py
 # This program asks a user to input a string and outputs every second letter in reverse order
-# Source: https://stackoverflow.com/questions/48873854/python-printing-ever-other-letter-of-a-word
+# Source: Jake VanderPlas, A Whirlwind tour of Python, page 33
 # Author: Anja Antolkovic
 
-#creating empty string
-secondString = str()
-# place the string here
-firstString = input("Please enter a sentence: ")
-for i in range(0,len(firstString)): # checking the condition
-  if i % 2 != 0: # there should be a remainder if we what to start from the last letter (index -1)
-      secondString += firstString[i] # adding the character to an empty string
-print(secondString[::-1]) # printing in reverse order
-
-
-
+firstString = input("Please enter a sentence: ") # place the string here
+secondString = firstString[::-2] # -2 is a step size
+print(secondString) # printing out the string
 
